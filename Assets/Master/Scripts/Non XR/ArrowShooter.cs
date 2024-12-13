5c939c325ed1de84912a27555a8a52f78f3cb4ab
Suman Raj <sumanr@synapseco.com>
2024-12-13 09:46:31 -0800

    initial commit

---
+++ Assets/Master/Scripts/Non XR/ArrowShooter.cs
@@ -0,0 +1,76 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowShooter : MonoBehaviour
+{
+    public GameObject arrowPrefab; // The arrow prefab
+    public Camera playerCamera; // The player's camera (child of the player or bow object)
+    public int poolSize = 10; // Number of arrows in the pool
+    public float shootForce = 30f; // The speed of the arrow when shot
+
+    private List<GameObject> arrowPool; // Pool of arrows
+
+    void Start()
+    {
+        // Initialize the arrow pool
+        arrowPool = new List<GameObject>();
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject arrow = Instantiate(arrowPrefab);
+            arrow.SetActive(false); // Deactivate the arrow initially
+            arrowPool.Add(arrow); // Add it to the pool
+        }
+    }
+
+    void Update()
+    {
+        if(GameManager.Instance.IsPlaying)
+        {
+            // Check if the right mouse button is clicked
+            if (Input.GetMouseButtonDown(0))
+            {
+                ShootArrow();
+            }
+        }
+    }
+
+    void ShootArrow()
+    {
+        // Get an inactive arrow from the pool
+        GameObject arrow = GetPooledArrow();
+
+        if (arrow != null)
+        {
+            // Set the arrow's position to the camera's position (center of the camera view)
+            arrow.transform.position = playerCamera.transform.position;
+
+            // Set the arrow's rotation to match the camera's forward direction
+            arrow.transform.rotation = playerCamera.transform.rotation;
+
+            // Activate the arrow
+            arrow.SetActive(true);
+
+            // Get the Rigidbody component to apply force
+            Rigidbody rb = arrow.GetComponent<Rigidbody>();
+
+            // Reset the arrow's velocity and apply force to shoot it forward in the direction the camera is facing
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            rb.AddForce(playerCamera.transform.forward * shootForce, ForceMode.Impulse);
+        }
+    }
+
+    // This method returns an inactive arrow from the pool
+    GameObject GetPooledArrow()
+    {
+        foreach (GameObject arrow in arrowPool)
+        {
+            if (!arrow.activeInHierarchy)
+            {
+                return arrow;
+            }
+        }
+        return null; // No inactive arrows available
+    }
+}
---
+++ Assets/ThirdParty/BNG Framework/Integrations/WebXR/WebXRInputProvider.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebXR;
+
+namespace BNG {
+    public class WebXRInputProvider : MonoBehaviour {
+
+        public WebXRController LeftController;
+        public WebXRController RightController;
+
+        void OnEnable() {
+            // Subscribe to input events
+            InputBridge.OnInputsUpdated += UpdateInputs;
+        }
+
+        void OnDisable() {
+            // Unsubscribe from input events
+            InputBridge.OnInputsUpdated -= UpdateInputs;
+        }
+
+        public virtual void UpdateInputs() {
+            // Override InputBridge inputs using WebXR API here
+            InputBridge.Instance.HMDActive = LeftController != null && (LeftController.isControllerActive || LeftController.isHandActive);
+
+            InputBridge.Instance.LeftTrigger = GetLeftTrigger();
+            InputBridge.Instance.RightTrigger = GetRightTrigger();
+
+            InputBridge.Instance.LeftGrip = GetLeftGrip();
+            InputBridge.Instance.RightGrip = GetRightGrip();
+
+            InputBridge.Instance.LeftThumbstickAxis = GetLeftThumbstickAxis();
+            InputBridge.Instance.RightThumbstickAxis = GetRightThumbstickAxis();
+
+            InputBridge.Instance.AButton = GetAButton();
+            InputBridge.Instance.BButton = GetBButton();
+            InputBridge.Instance.XButton = GetXButton();
+            InputBridge.Instance.YButton = GetYButton();
+
+            InputBridge.Instance.SupportsIndexTouch = false;
+            InputBridge.Instance.SupportsThumbTouch = false;
+        }
+
+        public Vector3 GetLeftThumbstickAxis() {
+            if (LeftController != null && LeftController.isControllerActive) {
+                return LeftController.GetAxis2D(WebXRController.Axis2DTypes.Thumbstick);
+            }
+
+            return Vector3.zero;
+        }
+
+        public Vector2 GetRightThumbstickAxis() {
+            if (RightController != null && RightController.isControllerActive) {
+                return RightController.GetAxis2D(WebXRController.Axis2DTypes.Thumbstick);
+            }
+
+            return Vector2.zero;
+        }
+
+        public float GetRightTrigger() {
+            if (RightController != null && RightController.isControllerActive) {
+                return RightController.GetAxis(WebXRController.AxisTypes.Trigger);
+            }
+
+            return 0;
+        }
+
+        public float GetLeftTrigger() {
+            if (LeftController != null && LeftController.isControllerActive) {
+                return LeftController.GetAxis(WebXRController.AxisTypes.Trigger);
+            }
+
+            return 0;
+        }
+
+        public float GetLeftGrip() {
+            if (LeftController != null && LeftController.isControllerActive) {
+                return LeftController.GetAxis(WebXRController.AxisTypes.Grip);
+            }
+
+            return 0;
+        }
+
+        public float GetRightGrip() {
+            if (RightController != null && RightController.isControllerActive) {
+                return RightController.GetAxis(WebXRController.AxisTypes.Grip);
+            }
+
+            return 0;
+        }
+
+        public bool GetAButton() {
+            if (RightController != null && RightController.isControllerActive) {
+                return RightController.GetButton(WebXRController.ButtonTypes.ButtonA);
+            }
+
+            return false;
+        }
+
+        public bool GetBButton() {
+            if (RightController != null && RightController.isControllerActive) {
+                return RightController.GetButton(WebXRController.ButtonTypes.ButtonB);
+            }
+
+            return false;
+        }
+
+        public bool GetXButton() {
+            if (LeftController != null && LeftController.isControllerActive) {
+                return LeftController.GetButton(WebXRController.ButtonTypes.ButtonA);
+            }
+
+            return false;
+        }
+
+        public bool GetYButton() {
+            if (LeftController != null && LeftController.isControllerActive) {
+                return LeftController.GetButton(WebXRController.ButtonTypes.ButtonB);
+            }
+
+            return false;
+        }
+    }
+}
